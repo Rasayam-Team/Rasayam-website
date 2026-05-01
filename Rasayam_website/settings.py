8efe012c69f29c8a082ab87630f593602674f1a9
Debabrat <debabratbehera616@gmail.com>
2026-05-01 19:06:35 +0530

    Fixed LOGIN_URL redirect and disabled debug mode

--- Rasayam_website/settings.py
+++ Rasayam_website/settings.py
@@ -236,3 +236,5 @@ UNFOLD = {
 # Fetching keys from the .env file you just created
 RAZORPAY_KEY_ID = os.getenv('RAZORPAY_KEY_ID')
 RAZORPAY_KEY_SECRET = os.getenv('RAZORPAY_KEY_SECRET')
+
+LOGIN_URL = 'login'
