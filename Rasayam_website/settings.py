0fd63bd80c8aeb7128f4c315a7d1989fdfeb0d1f
Debabrat <debabratbehera616@gmail.com>
2026-05-01 18:44:30 +0530

    Fixing STATIC_ROOT typo and path

--- Rasayam_website/settings.py
+++ Rasayam_website/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (Images uploaded via Admin/Boutique Portal)
 MEDIA_URL = '/media/'
